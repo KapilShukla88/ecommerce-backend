eb1f4fab79105340d0b17856ee0577ebed3d9b26
Kapil Shukla <57938801+KapilShukla88@users.noreply.github.com>
2024-07-03 15:49:09 +0530

    update the validator message of products

--- src/serializers/validators/product-validator.js
+++ src/serializers/validators/product-validator.js
@@ -10,106 +10,109 @@ export const addNewProductValidator = [
   body("name")
     .not()
     .isEmpty()
-    .withMessage("Please enter product name.")
+    .withMessage("Please provide the product name.")
     .isString()
-    .withMessage("name should be of string type")
+    .withMessage("The name must be a string.")
     .isLength({ max: 40 })
-    .withMessage("name should of max length 40"),
+    .withMessage("The name must have a maximum length of 40 characters."),
   body("description")
     .not()
     .isEmpty()
-    .withMessage("Please enter product description."),
+    .withMessage("Please provide a product description."),
   body("category")
     .not()
     .isEmpty()
-    .withMessage("Please enter product category")
+    .withMessage("Please specify the product category.")
     .trim()
     .isIn(PRODUCT_CATEGORY_ENUM)
     .withMessage(
-      `Category should be from these ${PRODUCT_CATEGORY_ENUM.join(", ")}`
+      `The category must be one of the following:${PRODUCT_CATEGORY_ENUM.join(", ")}`
     ),
   body("brand")
     .not()
     .isEmpty()
-    .withMessage("Please enter product brand name.")
+    .withMessage("Please provide the product brand name.")
     .trim()
     .isIn(PRODUCT_BRAND_ENUM)
     .withMessage(
-      `Brand should be from these brands ${PRODUCT_BRAND_ENUM.join(", ")}`
+      `The brand must be one of the following: ${PRODUCT_BRAND_ENUM.join(", ")}`
     ),
   body("price")
     .not()
     .isEmpty()
-    .withMessage("Please enter price of a product.")
+    .withMessage("Please enter the product price.")
     .isNumeric()
-    .withMessage("Price should be of number type")
+    .withMessage("The price must be a number.")
     .trim()
     .isLength({ max: 8 })
-    .withMessage("Price should be less than or equal to 8 digit."),
+    .withMessage("The price must be no more than 8 digits long."),
   body("stock")
     .not()
     .isEmpty()
-    .withMessage("Please enter number of stocks available.")
+    .withMessage("Please enter the number of stocks available.")
     .isNumeric()
-    .withMessage("Stocks should of integer type")
+    .withMessage("The stock quantity must be an integer.")
     .trim()
     .isLength({ max: 2 })
-    .withMessage("Stocks should not be more than or equal to 100"),
+    .withMessage("The stock quantity must be less than 100."),
   errorValidatorResponse,
 ];
 
 export const deleteProductValidator = [
-  param("productId").notEmpty().withMessage("Product id is required."),
+  param("productId").notEmpty().withMessage("A product ID is required."),
   errorValidatorResponse,
 ];
 
 export const updateProductValidator = [
-  param("productId").notEmpty().withMessage("Product id is required."),
-  body("name").optional().notEmpty().withMessage("Name field is required."),
+  param("productId").notEmpty().withMessage("A product ID is required."),
+  body("name").optional().notEmpty().withMessage("Please provide the product name.").isString()
+    .withMessage("The name must be a string.")
+    .isLength({ max: 40 })
+    .withMessage("The name must have a maximum length of 40 characters."),,
   body("description")
     .optional()
     .not()
     .isEmpty()
-    .withMessage("Please enter product description."),
+    .withMessage("Please provide a product description."),
   body("category")
     .optional()
     .not()
     .isEmpty()
-    .withMessage("Please enter product category")
+    .withMessage("Please specify the product category.")
     .trim()
     .isIn(PRODUCT_CATEGORY_ENUM)
     .withMessage(
-      `Category should be from these ${PRODUCT_CATEGORY_ENUM.join(", ")}`
+      `The category must be one of the following:${PRODUCT_CATEGORY_ENUM.join(", ")}`
     ),
   body("brand")
     .optional()
     .not()
     .isEmpty()
-    .withMessage("Please enter product brand name.")
+    .withMessage("Please provide the product brand name.")
     .trim()
     .isIn(PRODUCT_BRAND_ENUM)
     .withMessage(
-      `Brand should be from these brands ${PRODUCT_BRAND_ENUM.join(", ")}`
+      `The brand must be one of the following: ${PRODUCT_BRAND_ENUM.join(", ")}`
     ),
   body("price")
     .optional()
     .not()
     .isEmpty()
-    .withMessage("Please enter price of a product.")
+    .withMessage("Please enter the product price.")
     .isNumeric()
-    .withMessage("Price should be of number type")
+    .withMessage("The price must be a number.")
     .trim()
     .isLength({ max: 8 })
-    .withMessage("Price should be less than or equal to 8 digit."),
+    .withMessage("The price must be no more than 8 digits long."),
   body("stock")
     .not()
     .isEmpty()
     .withMessage("Please enter number of stocks available.")
     .isNumeric()
-    .withMessage("Stocks should of integer type")
+    .withMessage("The stock quantity must be an integer.")
     .trim()
     .isLength({ max: 2 })
-    .withMessage("Stocks should not be more than or equal to 100"),
+    .withMessage("The stock quantity must be less than 100."),
   errorValidatorResponse,
 ];
 
@@ -117,29 +120,29 @@ export const reviewValidator = [
   param("productId").notEmpty().withMessage("Product id is required."),
   body("rating")
     .notEmpty()
-    .withMessage("Please give at least 1 rating.")
+    .withMessage("Please provide at least one rating.")
     .isNumeric()
-    .withMessage("Rating should be of type number."),
+    .withMessage("The rating must be a number."),
   body("title")
     .notEmpty()
-    .withMessage("Title field should not be empty.")
+    .withMessage("The title field cannot be empty.")
     .isLength({ max: 30 })
-    .withMessage("Title should not exceed more than 30 characters"),
+    .withMessage("The title must not exceed 30 characters."),
   body("comment")
     .optional()
     .trim()
     .isLength({ max: 100 })
-    .withMessage("Comment should not exceed more than 50 characters."),
+    .withMessage("The comment must not exceed 50 characters."),
   errorValidatorResponse,
 ];
 
 export const getReviewValidator = [
-  param("productId").notEmpty().withMessage("Product id is required."),
+  param("productId").notEmpty().withMessage("Product Id is required."),
   errorValidatorResponse,
 ];
 
 export const deleteProductReviewValidator = [
-  param("productId").notEmpty().withMessage("Product id is required"),
-  param("id").notEmpty().withMessage("Review id is required."),
+  param("productId").notEmpty().withMessage("Product Id is required"),
+  param("id").notEmpty().withMessage("Review Id is required."),
   errorValidatorResponse,
 ];
