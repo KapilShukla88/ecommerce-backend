242edeb630c108b02fa32684ca4a4190215d92c6
Kapil Shukla <57938801+KapilShukla88@users.noreply.github.com>
2024-07-03 14:47:16 +0530

    added the comments on open routes index.js file

--- src/routes/open-routes/index.js
+++ src/routes/open-routes/index.js
@@ -4,8 +4,16 @@ import refreshTokenRoute from "./refresh-token-route.js";
 
 const router = express();
 
+/**
+  @description this route use to get all the products, reviews etc.
+  route - "/v1/main/products/*"
+*/
 router.use("/products", products);
 
+/**
+  @description to get and delete the refresh token
+  route - "/v1/main/refreshToken/*"
+*/
 router.use("/refreshToken", refreshTokenRoute);
 
 export default router;
