1e9a28e989a53e489512d98b8beadb2d9b435768
Kapil Shukla <57938801+KapilShukla88@users.noreply.github.com>
2024-07-03 15:50:04 +0530

    update the refresh token validator messages

--- src/serializers/validators/refresh-token-validator.js
+++ src/serializers/validators/refresh-token-validator.js
@@ -2,6 +2,6 @@ import { body } from "express-validator";
 import { errorValidatorResponse } from "./error-validator.js";
 
 export const refreshTokenValidator = [
-  body("refresh_token").not().isEmpty().withMessage("Refresh token require"),
+  body("refresh_token").not().isEmpty().withMessage("A refresh token is required."),
   errorValidatorResponse,
 ];
