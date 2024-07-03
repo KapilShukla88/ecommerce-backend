e004415bb77d8bf9dc3c52d7e6e339908d9c0b2f
Kapil Shukla <57938801+KapilShukla88@users.noreply.github.com>
2024-07-03 14:56:06 +0530

    add the comments on refresh token open route

--- src/routes/open-routes/refresh-token-route.js
+++ src/routes/open-routes/refresh-token-route.js
@@ -4,12 +4,26 @@ import refreshTokenController from "../../controller/refresh-token-controller.js
 
 const refreshTokenRoute = express();
 
+/**
+ * @description get the refresh token
+ * GET "v1/main/refreshToken"
+ * @Validator refreshTokenValidator validate whether refresh_token is passed on the body or not
+ * @body refresh_token
+ * Controller - return the verified refresh token
+ */
 refreshTokenRoute.get(
   "/",
   refreshTokenValidator,
   refreshTokenController.generateRefreshToken
 );
 
+/**
+ * @description to delete the refresh token
+ * GET "v1/main/refreshToken"
+ * @Validator refreshTokenValidator validate whether refresh_token is passed on the body or not
+ * @body refresh_token
+ * Controller - return the deleted refresh token
+ */
 refreshTokenRoute.delete(
   "/",
   refreshTokenValidator,
