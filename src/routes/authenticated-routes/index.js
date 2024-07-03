d9ea57a550b2cbb152c6a8fbd71edbecf89bf4a4
Kapil Shukla <57938801+KapilShukla88@users.noreply.github.com>
2024-07-03 14:58:03 +0530

    added comments on authenticated index.js file

--- src/routes/authenticated-routes/index.js
+++ src/routes/authenticated-routes/index.js
@@ -4,7 +4,15 @@ import productRoute from "./products-route.js";
 
 const authenticatedRoute = express();
 
+/**
+ * @description route for authentication
+ * GET "v1/main/auth"
+ */
 authenticatedRoute.use("/auth", authRoute);
+
+/**
+* @description authenticated products routes
+*/
 authenticatedRoute.use("/products", productRoute);
 
 export default authenticatedRoute;
